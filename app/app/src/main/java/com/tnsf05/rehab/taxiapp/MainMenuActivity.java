be5bb805c93834ebc04fd11859ee8e4cd31cd6b5
Assar Pettersson <mat13ape@student.lu.se>
2018-11-27 11:55:57 +0100

    ja

--- app/app/src/main/java/com/tnsf05/rehab/taxiapp/MainMenuActivity.java
+++ app/app/src/main/java/com/tnsf05/rehab/taxiapp/MainMenuActivity.java
@@ -1,29 +1,35 @@
 package com.tnsf05.rehab.taxiapp;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends ListActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    String tests[] = { "MapActivity", "PushActivity","ScheduleActivity"};
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests));
     }
 
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id){
+        super.onListItemClick(list,view,position,id);
+        String testName = tests[position];
+        try{
+            Class c = Class.forName("com.tnsf05.rehab.taxiapp." + testName);
+            Intent intent = new Intent(this, c);
+            startActivity(intent);
+        } catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
