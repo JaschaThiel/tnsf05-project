e758f7fffc56038c2d40bf83620675342e06056d
JaschaThiel <jascha.thiel@gmail.com>
2018-12-07 13:40:53 +0100

    Schedule view

--- app/app/src/main/java/com/tnsf05/rehab/taxiapp/ScheduleActivity.java
+++ app/app/src/main/java/com/tnsf05/rehab/taxiapp/ScheduleActivity.java
@@ -2,12 +2,42 @@ package com.tnsf05.rehab.taxiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ScheduleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ArrayList scheduleList = new ArrayList<String>();
+        scheduleList.add("Assar, 12:30");
+        scheduleList.add("Bengt, 13:00");
+        scheduleList.add("Charlie, 13:30");
+        scheduleList.add("David, 14:00");
+        scheduleList.add("Erik, 14:30");
+        scheduleList.add("Felix, 15:00");
+        scheduleList.add("Gunnar, 15:30");
+        scheduleList.add("Herbert, 16:00");
+        scheduleList.add("Isak, 16:30");
+        scheduleList.add("Jascha, 17:00");
+        scheduleList.add("Karl, 17:30");
+        scheduleList.add("Leopold, 18:00");
+        scheduleList.add("Max, 18:30");
+        scheduleList.add("Niclas, 19:00");
+        scheduleList.add("Ola, 19:30");
+        scheduleList.add("Peter, 20:00");
+        scheduleList.add("Stefan, 20:30");
+        scheduleList.add("Tore, 21:00");
+
         setContentView(R.layout.activity_schedule);
+        ListView mListView = (ListView) findViewById(R.id.mylistv);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, (String[]) scheduleList.toArray(new String[scheduleList.size()]));
+        mListView.setAdapter(adapter);
+
     }
 }
