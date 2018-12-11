81b7174e100c6c05434320c4c69ccb59cb556f98
Assar Pettersson <mat13ape@student.lu.se>
2018-12-11 13:02:42 +0100

    added notification history activity

--- app/app/src/main/java/com/tnsf05/rehab/taxiapp/NotificationHistory.java
+++ app/app/src/main/java/com/tnsf05/rehab/taxiapp/NotificationHistory.java
@@ -2,30 +2,29 @@ package com.tnsf05.rehab.taxiapp;
 
 import android.app.ListActivity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainMenuActivity extends ListActivity {
+import java.util.ArrayList;
 
-    String tests[] = { "PushActivity", "ScheduleActivity", "NotificationHistory"};
+public class NotificationHistory extends ListActivity {
+
+    String children[] = { "Bengt", "Filip", "Lars"};
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests));
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, children));
     }
 
     @Override
     protected void onListItemClick(ListView list, View view, int position, long id){
         super.onListItemClick(list,view,position,id);
-        String testName = tests[position];
+        String testName = children[position];
         try{
-            Class c = Class.forName("com.tnsf05.rehab.taxiapp." + testName);
+            Class c = Class.forName("com.tnsf05.rehab.taxiapp.children." + testName);
             Intent intent = new Intent(this, c);
             startActivity(intent);
         } catch(ClassNotFoundException e){
